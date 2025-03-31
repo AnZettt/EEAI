3e8fcb7f8b7719ff07c09597d0d0a39b703fb375
AnZettt <muazzam.syahdan@gmail.com>
2025-04-01 06:26:37 +0700

    Updating the cleaning process

--- collect.py
+++ collect.py
@@ -8,11 +8,11 @@ data = []
 
 for file in os.listdir(Data_Directory):
     print(file)
-    data.append(pd.read_csv(os.path.join(Data_Directory + '/' + file)))
+    data.append(pd.read_csv(os.path.join(Data_Directory + '/' + file), dtype=str))
 
 data = pd.concat(data)
 
-RQ = data['ResearchQuestion']
+RQ = data['ResearchQuestion'].astype(str)
 
 print('Total RQ: ', len(RQ))
 
@@ -23,7 +23,7 @@ print('Total unique RQ: ', len(RQ))
 clean = []
 
 for i in RQ:
-    tokens = i.split()
+    tokens = str(i).split()
     table = str.maketrans('', '', string.punctuation)
     tokens = [w.translate(table) for w in tokens]
     tokens = [word for word in tokens if word.isalpha()]
